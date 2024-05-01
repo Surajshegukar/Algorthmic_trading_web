9818f22e2dbae2e3d018943936fd14a902dfeedf
Surajshegukar <surajshegukar2732@gmail.com>
2024-05-02 01:50:08 +0530

    modified:   main.py

--- main.py
+++ main.py
@@ -14,11 +14,22 @@ uploaded_file = st.file_uploader("Upload CSV file", type=['csv'])
 if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
     st.write("Preview of uploaded data:")
-    st.write(data.head())
     
-    new_features = data[['RSI', 'k_percent', 'r_percent', 'Price_Rate_Of_Change', 'MACD', 'On Balance Volume']] 
+    last_row = data[(data['datetime'] == data['datetime'].max())]
+    st.write(last_row)
+    
+    new_features = last_row[['RSI', 'k_percent', 'r_percent', 'Price_Rate_Of_Change', 'MACD', 'On Balance Volume']]
     
     # # Make predictions
     predictions = loaded_model.predict(new_features)
     
-    st.write(predictions)
+    for i in range(len(predictions)):
+        prediction = predictions[i]
+        if str(prediction) == 1:
+            st.write("Predicted Signal: Buy")
+            st.write("the model is predicting a buy shares at the current price")
+        else:
+            st.write("Predicted Signal : Sell")
+            st.write("the model is predicting a sell shares at the current price")
+        
+    
